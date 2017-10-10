1c91c6bbb220c2e56a39a21bd07d0b4cfb24b2c2
yossi-r <yossi.ros@gmail.com>
2017-10-10 04:14:02 +0000

    cleaning

--- roles/testing/files/irules/bodgeit-rewrite.tcl
+++
@@ -1,5 +0,0 @@
-when HTTP_REQUEST {
-	if { [HTTP::host] equals "jc-demof5-wp01-pip.westus2.cloudapp.azure.com" and [HTTP::uri] starts_with "/broken" } {
-		HTTP::redirect "http://isitthef5.com"
-	}
-}
